1d0e2d40a08253e00cd7ea476316f5dd472f5589
Pushpaindu Nath <pushpaindunath@gmail.com>
2026-09-05 21:34:33 +0530

    ci(mobile): add diagnostic failure log dump and refine metro config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -9,7 +9,7 @@ const config = getDefaultConfig(projectRoot);
 // 1. Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 
-// 2. Let Metro know where to resolve packages and in what order in a PNPM monorepo
+// 2. Let Metro know where to resolve packages in a PNPM monorepo
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(monorepoRoot, "node_modules"),
